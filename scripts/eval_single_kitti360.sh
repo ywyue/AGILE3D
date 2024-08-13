bf28728bc88429e6a10d297fc684f3462660b005
Yuanwen Yue <865051699@qq.com>
2024-08-14 00:49:53 +0100

    fix typo

--- scripts/eval_single_kitti360.sh
+++ scripts/eval_single_kitti360.sh
@@ -5,6 +5,6 @@ python eval_single_obj.py --dataset=kitti360 \
                --scan_folder=data/KITTI360/single/crops \
                --crop \
                --val_list=data/KITTI360/single/object_ids.npy \
-               --val_list_classes=data/KITTI360/single/object_classes.npy \
+               --val_list_classes=data/KITTI360/single/object_classes.txt \
                --output_dir=results/KITTI_single \
                --checkpoint=weights/checkpoint1099.pth
